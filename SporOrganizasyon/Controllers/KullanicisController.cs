d2c7b4f2f96cb912901b1ada4e6165e2beb1148d
semvs <semvs@ayvansaray.edu.tr>
2019-02-20 14:22:26 +0300

    changes

---
+++ SporOrganizasyon/Controllers/KullanicisController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SporOrganizasyon.Models;
+
+namespace SporOrganizasyon.Controllers
+{
+    public class KullanicisController : Controller
+    {
+        private SporOEntities db = new SporOEntities();
+
+        // GET: Kullanicis
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        // GET: Kullanicis/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kullanici kullanici = db.Kullanici.Find(id);
+            if (kullanici == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kullanici);
+        }
+
+        // GET: Kullanicis/Create
+        public ActionResult Create()
+        {
+            var sporlar = db.Sporlar.ToList();
+            return View(Tuple.Create<Kullanici, List<Sporlar>>(new Kullanici(), sporlar));
+        }
+
+        // POST: Kullanicis/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Kid,Ad,Soyad,Email,Telefon,Sifre,Ilce,DogumTarihi,Cinsiyet,isLogin")] Kullanici kullanici)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kullanici.Add(kullanici);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kullanici);
+        }
+
+        // GET: Kullanicis/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kullanici kullanici = db.Kullanici.Find(id);
+            if (kullanici == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kullanici);
+        }
+
+        // POST: Kullanicis/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Kid,Ad,Soyad,Email,Telefon,Sifre,Ilce,DogumTarihi,Cinsiyet,isLogin")] Kullanici kullanici)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kullanici).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kullanici);
+        }
+
+        // GET: Kullanicis/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kullanici kullanici = db.Kullanici.Find(id);
+            if (kullanici == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kullanici);
+        }
+
+        // POST: Kullanicis/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kullanici kullanici = db.Kullanici.Find(id);
+            db.Kullanici.Remove(kullanici);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
